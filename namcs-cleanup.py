29ceccb2b9b4dd2aad62cc4f5d013ac2ba7347a5
Kier von Konigslow <kvonkonigslow@gmail.com>
2021-02-26 11:12:26 -0500

    Added column, header info ro df

--- namcs-cleanup.py
+++ namcs-cleanup.py
@@ -1,19 +1,27 @@
 import os, sys
 import pandas as pd
 
+def range_to_tuple(range):
+    x = range.split('-', 1)
+    if len(x) < 2:
+        return (int(x[0])-1, int(x[0]))
+    else:
+        return (int(x[0])-1, int(x[1]))
+
 cols = pd.read_csv(
-    os.path.join('cols', 'tabula-namcs2016.tsv')
+    os.path.join('cols', 'namcs2016.tsv'),
+    sep='\t',
+    header=0,
+    converters={'file_location': range_to_tuple}
 )
+
+ranges = cols['file_location'].tolist()
+headers = cols['item_description'].tolist()
 
 df = pd.read_fwf(
-    os.path.join('data', 'namcs2016.data'), 
-    [
-        (0, 2),
-        (2, 3),
-        (3, 6),
-        (6, 7),
-        (7, 10),
-    ]
+    os.path.join('data', 'namcs2016.data'),
+    colspecs=ranges,
+    names=headers,
 )
 
-print(cols)
+print(df)
